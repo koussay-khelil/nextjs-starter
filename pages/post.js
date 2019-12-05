ffb05797e381b845661307fe937c7ae247c5148f
koussay khelil <khelil.koussay@gmail.com>
2019-12-05 14:39:18 +0100

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import {withRouter} from 'next/router'
 
 const Post =  ({router}) => (
     <Layout title={router.query.title}>
-        <p style={{width: '80 vw'}}>this is a test</p>
+        <p style={{width: '80 vw'}}>You can write the blog here </p>
         
     </Layout>
 )

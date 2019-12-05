c4a6a7493b45955b054b57e3c87e1b91548bcf2f
koussay khelil <khelil.koussay@gmail.com>
2019-12-05 14:36:32 +0100

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -3,6 +3,6 @@ import Layout from '../components/Layout'
 
 export default ({statusCode}) => (
     <Layout title='error'>
-        {statusCode ? `Could not load data status code ${statusCode}` : `Couldn't get that page Sorry` }
+        {statusCode ? `Could not load data the status code is ${statusCode}` : `Couldn't get that page Sorry` }
     </Layout>
 )

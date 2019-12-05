a1e78559b3afdb52bf97b6aa363a0d5e22ae2bcd
koussay khelil <khelil.koussay@gmail.com>
2019-12-05 14:35:01 +0100

    Update buyMeCoffee.js

--- pages/buyMeCoffee.js
+++ pages/buyMeCoffee.js
@@ -2,6 +2,6 @@ import Layout from '../components/Layout'
 
 export default  () => (
     <Layout title='Hire Me'>
-        <p>You can hire me at <a href='mailto:khelil.koussay@gmail.com'>my email</a> </p>
+        <p>You can buy me coffee at <a href='mailto:khelil.koussay@gmail.com'>my email</a> </p>
     </Layout>
 )
